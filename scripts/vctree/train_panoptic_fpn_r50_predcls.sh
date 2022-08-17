7357e711c60793efcbf61f0e4889ee0f2608efd4
hexianhua <hexianhua@meituan.com>
2022-08-17 22:09:40 +0800

    vctree 2GPUs

--- scripts/vctree/train_panoptic_fpn_r50_predcls.sh
+++ scripts/vctree/train_panoptic_fpn_r50_predcls.sh
@@ -10,6 +10,15 @@ jobname=openpsg
 #PYTHONPATH='.':$PYTHONPATH \
 #srun -p dsta --mpi=pmi2 --gres=gpu:${GPU} -n1 --ntasks-per-node=${GPU} \
 #--kill-on-bad-exit=1 --job-name=${jobname} -w SG-IDC1-10-51-2-${node} \
+#PYTHONPATH='.':$PYTHONPATH \
+#python tools/train.py \
+#  configs/vctree/panoptic_fpn_r50_fpn_1x_predcls_psg.py
+
+
 PYTHONPATH='.':$PYTHONPATH \
-python tools/train.py \
-  configs/vctree/panoptic_fpn_r50_fpn_1x_predcls_psg.py
+python -m torch.distributed.launch \
+--nproc_per_node=2 --master_port=29500 \
+  tools/train.py \
+  configs/vctree/panoptic_fpn_r50_fpn_1x_predcls_psg.py \
+  --gpus 2 \
+  --launcher pytorch
